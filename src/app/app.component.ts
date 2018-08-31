e20b31a373e2a928a18077fd499be494ef1ddd69
Grzegorz <kalarus.g@gmail.com>
2018-08-31 14:43:21 +0200

    todo-angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  newTask: string;
+  taskList: Array<string> = [];
+  taskDone: Array<string> = [];
+
+  add() {
+    this.taskList.push(this.newTask);
+    this.newTask = '';
+    console.log(this.taskList);
+  }
+
+  remove(task: string) {
+    this.taskList = this.taskList.filter(item => item !== task);
+  }
+
+  done(task: string) {
+    this.taskDone.push(task);
+    this.remove(task);
+  }
+}
